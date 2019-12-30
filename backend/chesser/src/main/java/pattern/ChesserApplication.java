4c0cf32089fda8cbb9fa5e100638a00df6495fed
Emmanuel Telewa <emmanuel.telewa@jumo.world>
2019-12-30 14:45:26 +0300

    Remove old unused files

--- backend/chesser/src/main/java/pattern/ChesserApplication.java
+++ backend/chesser/src/main/java/pattern/ChesserApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChesserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChesserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChesserApplication.class, args);
+    }
 
 }
